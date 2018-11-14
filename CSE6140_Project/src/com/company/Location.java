aaa0e57f38741801c5181f5c2a3116e297aa6585
Xuncheng Zhou <43225934+Williamzxc1996@users.noreply.github.com>
2018-11-14 12:29:26 -0500

    Add files via upload

---
+++ CSE6140_Project/src/com/company/Location.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Location {
+    public int locId;
+    public double x;
+    public double y;
+
+    public Location(int id, double x, double y){
+        this.locId = id;
+        this.x = x;
+        this.y = y;
+    }
+}
